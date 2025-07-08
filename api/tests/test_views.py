12356ed0e4504f2d4e10fe6052cb5fad4862a4b0
vitor <vitorpereira032@gmail.com>
2025-07-08 13:36:44 -0300

    feat: Etapa 3 - Configura o pipeline de CI com build, test e lint

---
+++ api/tests/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+
+class TestPing(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_ping_authenticated(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('now', response.json())
---
+++ www/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\.(css|less|scss|sass)$_': 'identity-obj-proxy'
+  }
+};
