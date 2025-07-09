cfee59d19060c5f0645776ee2490db680aba137a
vitor <vitorpereira032@gmail.com>
2025-07-09 15:37:45 -0300

    fix(www): corrige jest.config e usa babel.config.js existente

--- www/jest.config.js
+++ www/jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
-    '\.(css|less|scss|sass)$_': 'identity-obj-proxy'
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
   transform: {
-    '^.+\.(js|jsx|ts|tsx)$_': 'babel-jest',
-  },
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  }
 };
