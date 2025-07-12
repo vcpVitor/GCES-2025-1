7dc8309653f928f5ec58a41d3e5478bdbba353f5
vitor <vitorpereira032@gmail.com>
2025-07-12 14:56:24 -0300

    Configura ESLint para frontend e corrige lint em _app.js

---
+++ www/.eslintrc.js
@@ -0,0 +1,24 @@
+// www/.eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- www/pages/_app.js
+++ www/pages/_app.js
@@ -1,10 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { AuthProvider } from "../auth";
 import "../styles.css";
+
 function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
 export default App;
